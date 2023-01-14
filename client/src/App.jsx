85a924659fc4e781cff6e1286332ca8d9555349d
Ko.Kko <ko.kko@mrm.ru>
2023-01-14 14:11:04 +0300

    set up routing2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import CartPage from './pages/CartPage';
 
 // import pizzas from './assets/pizza.json';
 
@@ -16,6 +17,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
